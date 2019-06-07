0025e30554f6bc56ca4a3e8c40ff6c2e0ec49260
Carmen Kwan <c27kwan@edu.uwaterloo.ca>
2019-06-07 15:01:50 -0700

    Redid the logic for emitting stackmaps

--- cranelift-codegen/src/regalloc/stackmaps.rs
+++ cranelift-codegen/src/regalloc/stackmaps.rs
@@ -1,33 +1,74 @@
 use crate::cursor::{Cursor, FuncCursor};
 use crate::dominator_tree::DominatorTree;
-use crate::ir::Function;
-use crate::ir::InstBuilder;
+use crate::ir::{Ebb, Function, InstBuilder, Value};
 use crate::regalloc::live_value_tracker::LiveValueTracker;
 use crate::regalloc::liveness::Liveness;
 use std::collections::HashSet;
 use std::vec::Vec;
 
+fn get_live_ref_values<'f>(tracker: &mut LiveValueTracker, pos: &FuncCursor<'f>) -> Vec<Value> {
+    // Grab the values that are still live
+    let live_info = tracker.live();
+
+    // Iterate through all live values, return only the references
+    let mut live_ref_values = Vec::new();
+
+    for value_in_list in live_info {
+        if pos.func.dfg.value_type(value_in_list.value).is_ref() {
+            live_ref_values.push(value_in_list.value);
+        }
+    }
+
+    live_ref_values
+}
+
+fn try_insert_savepoint_at_ebb_top<'f>(
+    ebb: Ebb,
+    pos: &mut FuncCursor<'f>,
+    liveness: &mut Liveness,
+    domtree: &mut DominatorTree,
+    tracker: &mut LiveValueTracker,
+    dest_ebbs: &HashSet<Ebb>
+) {
+    // Analyse liveness of variables from the top of ebb
+    tracker.ebb_top(ebb, &pos.func.dfg, liveness, &pos.func.layout, domtree);
+    tracker.drop_dead_params();
+
+    let live_ref_values = get_live_ref_values(tracker, &pos);
+
+    // If the current ebb is in the hashset, there is a jump to this cursor pos
+    // from an instruction positioned further down in layout. All loops will lead
+    // to such a jump (the converse is not true), so insert stackmap here.
+    if dest_ebbs.contains(&ebb) && !live_ref_values.is_empty() { 
+        pos.goto_first_inst(ebb);
+        pos.ins().stackmap(&live_ref_values);
+    }
+}
+
 // The emit_stackmaps() function analyzes each instruction to retrieve the liveness of
-// the defs and operands by traversing the dominator tree in a post order fashion.
+// the defs and operands by traversing a function's ebbs in reverse layout order.
 pub fn emit_stackmaps(
     func: &mut Function,
     domtree: &mut DominatorTree,
     liveness: &mut Liveness,
     tracker: &mut LiveValueTracker,
 ) {
-    // Visit EBBs in post-order
-    let mut pos = FuncCursor::new(func);
-    let mut ebbs_for_stackmap = HashSet::new();
+    let mut dest_ebbs: HashSet<Ebb> = HashSet::new();
+    let mut curr = func.layout.last_ebb();
 
-    for &ebb in domtree.cfg_postorder() {
-        // call ebb_top && drop_dead_params
-        tracker.ebb_top(ebb, &pos.func.dfg, liveness, &pos.func.layout, domtree);
-        tracker.drop_dead_params();
+    // Visit EBBs in reverse layout order
+    while let Some(ebb) = curr {
+        let mut self_loop = false;
+        let mut pos = FuncCursor::new(func);
+
+        try_insert_savepoint_at_ebb_top(ebb, &mut pos, liveness, domtree, tracker, &dest_ebbs);
 
         // From the top of the ebb, step through the instructions
         pos.goto_top(ebb);
 
         while let Some(inst) = pos.next_inst() {
+            let live_ref_values = get_live_ref_values(tracker, &pos);
+
             // Get opcode of instruction
             let opcode = pos.func.dfg[inst].opcode();
 
@@ -37,55 +78,20 @@ pub fn emit_stackmaps(
             // Get rid of values that have either (1) Dead Definitions or (2) Killed by Inst
             tracker.drop_dead(inst);
 
-            // create an empty vector to store the live values in
-            let mut live_value_list = Vec::new();
-
-            // Grab the values that are still live
-            let live_info = tracker.live();
-
-            if live_info.len() != 0 {
-                for value_in_list in live_info {
-                    // only store values that are reference types
-                    if pos.func.dfg.ctrl_typevar(inst).is_ref() {
-                        live_value_list.push(value_in_list.value);
-                    }
+            if let Some(dest) = pos.func.dfg[inst].branch_destination() {
+                // Loop within ebb, but savepoint instr was never added. Flag to handle this at the end.
+                if dest == ebb  && !dest_ebbs.contains(&ebb) {
+                    self_loop = true;
                 }
-            }
-
-            // Check if it's a branch instruction
-            if opcode.is_branch() {
-                // TODO: The stackmap instruction should only be added if the
-                // branch_dest is above the branch in the layout
-
-                // Find what the branch destination is
-                let branch_dest = pos.func.dfg[inst].branch_destination();
-
-                // Check if the branch destination is already in hash set
-                if ebbs_for_stackmap.contains(&branch_dest) {
-                    // If the destination is in the HashSet, we should replace the values
-                    // that the stackmap instruction currently holds
-                    pos.goto_first_insertion_point(branch_dest.unwrap());
-
-                    // remove the current stackmap instruction with old values
-                    pos.remove_inst();
-
-                    // insert new stackmap instruction with new values
-                    pos.ins().stackmap(&live_value_list);
-                } else {
-                    // If it isn't in the HashSet, we can insert stackmap instruction
-                    // without any worries. We also insert the destination into HashSet
-                    pos.goto_first_insertion_point(branch_dest.unwrap());
-                    pos.ins().stackmap(&live_value_list);
-
-                    ebbs_for_stackmap.insert(branch_dest);
-                }
-            }
-
-            // Check if it's a call instruction
-            if opcode.is_call() {
-                // insert stackmap instruction
-                pos.ins().stackmap(&live_value_list);
+                // Add destination branch to hashset
+                dest_ebbs.insert(dest);
+            } else if opcode.is_call() && !live_ref_values.is_empty() {
+                pos.ins().stackmap(&live_ref_values);
             }
         }
+        if self_loop {
+            try_insert_savepoint_at_ebb_top(ebb, &mut pos, liveness, domtree, tracker, &dest_ebbs);
+        }
+        curr = func.layout.prev_ebb(ebb);
     }
 }
